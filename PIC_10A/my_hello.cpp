5dc9111a875cb3b360c69b6e27f2fe248718836e
Daniel Ragsdale <daniel@studiodan.io>
2017-01-11 14:18:27 -0800

    Homework 0

---
+++ PIC_10A/my_hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout 
+		<< "Hello world! \n"
+		<< "What is my name? \n"
+		<< "My name is Daniel Ragsdale. \n"
+		<< "A wise man said \"Always code as if the \n"
+		<< "guy who ends up maintaining your code will be \n"
+		<< "a violent psycopath who knows where you live.\"." << endl;
+}
