7e63837cbd5d75458b3947649d0089769b1f0024
Daniel Ragsdale <ragsd77194@gmail.com>
2017-05-22 11:45:46 -0700

    Added comments. Fixed some small bugs

--- PIC_10B/HW5/ComplexDouble.h
+++ PIC_10B/HW5/ComplexDouble.h
@@ -3,19 +3,53 @@
 
 #include<ostream>
 
+/**
+* A class reprsenting a complex number with doubles for elements
+**/
 class ComplexDouble
 {
 	public:
+		/**
+		* The real part of this ComplexDouble
+		**/
 		double mRealPart;
+		
+		/**
+		* The coefficient of the imaginary term
+		**/
 		double mImaginaryPart;
 		
+		/**
+		* Create a ComplexDouble with 0 for both the real and imaginary components
+		**/
 		ComplexDouble() : mRealPart(0), mImaginaryPart(0) {}
-		ComplexDouble(double rp, double ip) : mRealPart(rp), mImaginaryPart(ip) {}
-		ComplexDouble(double rp) : mRealPart(rp), mImaginaryPart(0) {}
 
+		/**
+		* Create a ComplexDouble with real part rp and imaginary part 0.
+		*
+		* @param rp the real part of this number
+		**/
+		ComplexDouble(double rp) : mRealPart(rp), mImaginaryPart(0) {}
+		
+		/**
+		* Create a ComplexDouble with the specified real and imaginary parts
+		*
+		* @param rp the real part of this number
+		* @param ip the imaginary part of this number
+		**/
+		ComplexDouble(double rp, double ip) : mRealPart(rp), mImaginaryPart(ip) {}
+
+		/**
+		* Create the complex conjugate of this number.
+		*
+		* @return the complex conjugate of this ComplexDouble
+		**/
 		ComplexDouble conjugate() const;
 };
 
+/**
+* Operators are overloaded according to their mathematical definitions.
+**/
 ComplexDouble operator+(const ComplexDouble& lhs, const ComplexDouble& rhs);
 ComplexDouble operator-(const ComplexDouble& lhs, const ComplexDouble& rhs);
 ComplexDouble operator*(const ComplexDouble& lhs, const ComplexDouble& rhs);
